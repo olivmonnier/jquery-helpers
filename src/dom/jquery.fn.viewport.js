73e2609de3380d1657dc2c695c19d71d9310e448
Olivier Monnier <omonnier.dev@gmail.com>
2017-03-31 18:40:44 +0200

    add

--- src/dom/jquery.fn.viewport.js
+++ src/dom/jquery.fn.viewport.js
@@ -1,23 +1,17 @@
 jQuery.fn.viewport = function (options, fn) {
-		var min = options.min || null;
-		var max = options.max || null;
-		var el = this;
+	var min = options.min || null;
+	var max = options.max || null;
+	var self = this;
 
-		if (min || max) {
-			$(window).resize(function () {
-				var width = $(window).width();
+	if (min || max) {
+		$(window).resize(function () {
+			var width = $(window).width();
 
-				if (min && width >= min) {
-					if (max) {
-						if (width <= max) fn.call(el);
-					} else {
-						fn.call(el);
-					}
-					return;
-				}
-				if (max && width <= max) fn.call(el);
-			});
-			$(window).trigger('resize');
-			return el;
-		}
+			if (min && width >= min) return fn.call(self);
+			if (max && width <= max) return fn.call(self);
+		});
+		$(window).trigger('resize');
+		
+		return self;
 	}
+}
