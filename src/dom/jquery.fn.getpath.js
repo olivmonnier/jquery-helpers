9d98b2754ccca9d5bba62a3d99a1e8958c3c4ca8
Olivier Monnier <omonnier.dev@gmail.com>
2017-11-27 17:40:47 +0100

    Update jquery.fn.getpath.js

--- src/dom/jquery.fn.getpath.js
+++ src/dom/jquery.fn.getpath.js
@@ -1,11 +1,24 @@
-jQuery.fn.getPath = function(asArr) {
-  var path = this.parents().andSelf().map(function () {
-    var $this = $(this),
-    tagName = this.nodeName.toLowerCase();
-    if ($this.siblings(tagName).length > 0) {
-      tagName += ":eq(" + $this.prevAll(tagName).length + ")";
+jQuery.fn.getPath = function() {
+  if (this.length != 1) throw 'Requires one element.';
+
+  let path, node = this;
+
+  while (node.length) {
+    let realNode = node[0], name = realNode.localName;
+
+    if (!name) break;
+    name = name.toLowerCase();
+
+    const parent = node.parent();
+    const siblings = parent.children(name);
+
+    if (siblings.length > 1) {
+      name += ':eq(' + siblings.index(realNode) + ')';
     }
-    return tagName;
-  }).get();
-  return asArr ? path : path.join(">");
+
+    path = name + (path ? '>' + path : '');
+    node = parent;
+  }
+
+  return path;
 }
