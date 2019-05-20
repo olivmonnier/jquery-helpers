443e749b73ee006831047966c5fbbc88a05d1f20
Olivier Monnier <monnier@indigo-net.com>
2019-05-20 17:59:12 +0200

    WIP

--- src/dom/$.fn.getPath.js
+++ src/dom/$.fn.getPath.js
@@ -2,16 +2,16 @@
   $.fn.getPath = function() {
     if (this.length != 1) throw 'Requires one element.';
   
-    let path, node = this;
+    var path, node = this;
   
     while (node.length) {
-      let realNode = node[0], name = realNode.localName;
+      var realNode = node[0], name = realNode.localName;
   
       if (!name) break;
       name = name.toLowerCase();
   
-      const parent = node.parent();
-      const siblings = parent.children(name);
+      var parent = node.parent();
+      var siblings = parent.children(name);
   
       if (siblings.length > 1) {
         name += ':eq(' + siblings.index(realNode) + ')';
